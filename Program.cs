8f7daa51d0057da7f424824ca3521e9647bb8b76
Eduardo.B <g.eduardobaloti@gmail.com>
2022-04-21 14:49:33 -0300

    App finished

--- Program.cs
+++ Program.cs
@@ -8,49 +8,49 @@ class FileApp{
         DateTime lastRun = DateTime.Parse(timestamp);
         if (DateTime.Now > lastRun.AddMinutes(1))
         {
-        //First Step - > receive a parameter
-        Console.Write("Input the file location: ");
-        string path = Console.ReadLine();
+            //First Step - > receive a parameter
+            Console.Write("Input the file location: ");
+            string path = Console.ReadLine();
 
-        //Second Step -> file processing
-        List<string> users = new List<string>();
-        using (StreamReader sr = File.OpenText(path))
-        {
-            string s = "";
-            while ((s = sr.ReadLine()) != null)
+            //Second Step -> file processing
+            List<string> users = new List<string>();
+            using (StreamReader sr = File.OpenText(path))
             {
-                users.Add(s);
-                Console.WriteLine(s);
-            }
-        };
-
-        //Third Step -> Api retrieve
-        HttpClient client = new HttpClient();
-        for (var i = 0; i <= users.Count-1; i++)
-        {
-            string user = users[i];
-            string url = ("https://api.bitbucket.org/2.0/workspaces/"+user);
-            try
-            {
-                string response = await client.GetStringAsync(url);
-                Console.WriteLine(url);
-                Console.WriteLine(response);
-                await Task.Delay(5000);
-                //Whrite in LogFile
-                using (StreamWriter writer = new StreamWriter("logfile.txt", true))   
-                {  
-                    writer.WriteLine(response.Trim());
+                string s = "";
+                while ((s = sr.ReadLine()) != null)
+                {
+                    users.Add(s);
+                    Console.WriteLine(s);
                 }
+            };
+
+            //Third Step -> Api retrieve
+            HttpClient client = new HttpClient();
+            for (var i = 0; i <= users.Count-1; i++)
+            {
+                string user = users[i];
+                string url = ("https://api.bitbucket.org/2.0/workspaces/"+user);
+                try
+                {
+                    string response = await client.GetStringAsync(url);
+                    Console.WriteLine(url);
+                    Console.WriteLine(response);
+                    await Task.Delay(5000);
+                    //Whrite in LogFile
+                    using (StreamWriter writer = new StreamWriter("logfile.txt", true))   
+                    {  
+                        writer.WriteLine(response.Trim());
+                    }
+                }
+                catch { }
             }
-            catch { }
-        }
-        File.WriteAllText("timelock.txt", DateTime.Now.ToString());
-        Thread.Sleep(5000);
-        }
-        else
-        {
-            Console.WriteLine("wait one minute to use again");
-        }
+            File.WriteAllText("timelock.txt", DateTime.Now.ToString());
+            Thread.Sleep(5000);
+         }
+         else
+         {
+             Console.WriteLine("wait one minute to use again");
+         }
 	}
 };
 
