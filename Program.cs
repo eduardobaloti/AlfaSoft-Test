fa68de042d5bb5fbe1f157ce7a68ba302061ea5b
Eduardo.B <g.eduardobaloti@gmail.com>
2022-04-21 09:23:20 -0300

    base app

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Hello, World!");
+//First Step - > receive a parameter
+
+//Second Step -> file processing
+
+//Third Step -> Api retrieve
+
+//Fouth Step -> Generate a log file
+
+
